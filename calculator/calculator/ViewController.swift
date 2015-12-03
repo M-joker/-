d9118cb8f5f7ee9f9cca644afb1316619c2c07c0
M-joker <alislwd@icloud.com>
2015-12-03 12:37:11 +0800

    wrote √

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -31,23 +31,27 @@ class ViewController: UIViewController
         }
         switch operation {
         case "*": performOperation { $0 * $1 }
-        case "/": performOperation { $0 / $1 }
+        case "÷": performOperation { $1 / $0 }
         case "+": performOperation { $0 + $1 }
-        case "-": performOperation { $0 - $1 }
+        case "-": performOperation { $1 - $0 }
+        case "√":operformOperation { sqrt($0) }
         default: break
         }
     }
-    
-    func performOperation(operation: (Double,Double) ->Double) {
-        if operandStack.count >= 2{
-            displayValue = operandStack.removeLast() * operandStack.removeLast()
+    func performOperation(operation :(Double,Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
         }
+    }
+    
+    func operformOperation(operation : Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
 
-    }
-    func multiply(op1: Double,op2: Double) ->Double {
-        return op1 * op2
-    }
     var operandStack = Array<Double>()
 
     @IBAction func enter() {
